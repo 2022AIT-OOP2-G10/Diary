9dcf2f51f08b2d2f6c8206c460f432de330bacb8
k21091 <k21091kk@aitech.ac.jp>
2022-12-08 15:40:22 +0900

    最初のコミット

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from diaries.DiarySample import DiarySample
+from diaries.Diary_k21091 import Diary_k21091
 
 #のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [
+    DiarySample(),
+    Diary_k21091(),
+    ]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
