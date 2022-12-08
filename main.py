bff3dd5621c678f3fcfba66df3d4dd9ee389a64a
NoisyShop <x18091xx@aitech.ac.jp>
2022-12-08 16:14:40 +0900

    村松

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+# -*- coding: utf-8 -*-
 from diaries.DiarySample import DiarySample
+<<<<<<< Updated upstream
 from diaries.Diary_k21091 import Diary_k21091
+=======
+from diaries.MuramatsuDiary import MuramatsuDiary
+>>>>>>> Stashed changes
 
 #のリストには、メンバーの各日記が格納されます。
 diaries = [
+<<<<<<< Updated upstream
     DiarySample(),
     Diary_k21091(),
     ]
+=======
+    DiarySample(), 
+    MuramatsuDiary(),
+]
+>>>>>>> Stashed changes
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
