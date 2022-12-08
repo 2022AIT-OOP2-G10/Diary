36371024a3f66d4d67d9ebee5f3eb8e0cfc6ff19
Kchanko <k21014kk@aitech.ac.jp>
2022-12-08 16:07:14 +0900

    k21014日記

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.k21014Diary import k21014Diary
 
 #↓のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(), 
+    k21014Diary(),
 ]
 for d in diaries:
     print("---------------------------------")
