9ac1d7758e8f66f548482567a477b1c319c58a95
k21091 <120067169+k21091@users.noreply.github.com>
2022-12-08 18:14:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from diaries.K21014Diary import K21014Diary
 from diaries.SomekiDiary import SomekiDiary
 from diaries.MuramatsuDiary import MuramatsuDiary
 from diaries.Diary_k21091 import Diary_k21091
-from diaries.k21054Diary import k21054Diary
+from diaries.K21054Diary import K21054Diary
 
 #↓のリストには、メンバーの各日記が格納されます。
 diaries = [
@@ -12,7 +12,7 @@ diaries = [
     SomekiDiary(),
     MuramatsuDiary(),
     Diary_k21091(),
-    k21054Diary(),
+    K21054Diary(),
 ]
 
 
