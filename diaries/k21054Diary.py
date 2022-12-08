2153026ab28c60dd6e5629a1b67a75e261c98691
k21091 <k21091kk@aitech.ac.jp>
2022-12-08 18:21:20 +0900

    Update k21054Diary.py

--- diaries/k21054Diary.py
+++ diaries/k21054Diary.py
@@ -1,12 +1,12 @@
-from diaries.K21054Diary import AbstractDiary
+from diaries.AbstractDiary import AbstractDiary
 
 class K21054Diary(AbstractDiary):
 
-    def get_data(self):
+    def get_date(self):
         return "2022-10-10"
 
     def get_summary(self):
-        return """良い1日だった。"""
+        return "良い1日だった。"
 
     def get_author(self):
         return "k21054"
