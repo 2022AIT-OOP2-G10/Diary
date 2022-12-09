68075d35b770a18b77e05ea81a035ce0af658d68
OkamotoAi <okamotoai0217@icloud.com>
2022-12-09 17:33:13 +0900

    add okamoto diary

---
+++ diaries/K20025Diary.py
@@ -0,0 +1,9 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class  K20025Diary(AbstractDiary):
+    def get_date(self):
+        return "2021-12-09"
+    def get_summary(self):
+        return "ご飯がおいしかった"
+    def get_author(self):
+        return "K20025"
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from diaries.K21014Diary import K21014Diary
 from diaries.SomekiDiary import SomekiDiary
 from diaries.MuramatsuDiary import MuramatsuDiary
 from diaries.Diary_k21091 import Diary_k21091
-from diaries.K21054Diary import K21054Diary
+from diaries.k21054Diary import K21054Diary
+from diaries.K20025Diary import K20025Diary
 
 #↓のリストには、メンバーの各日記が格納されます。
 diaries = [
@@ -13,6 +14,7 @@ diaries = [
     MuramatsuDiary(),
     Diary_k21091(),
     K21054Diary(),
+    K20025Diary()
 ]
 
 
