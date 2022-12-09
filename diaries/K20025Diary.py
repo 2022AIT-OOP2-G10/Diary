58c4c8a4788a2bfcef767a98be7b3bf814087133
k21091 <120067169+k21091@users.noreply.github.com>
2022-12-09 20:03:35 +0900

    Revert "add okamoto diary"

--- diaries/K20025Diary.py
+++
@@ -1,9 +0,0 @@
-from diaries.AbstractDiary import AbstractDiary
-
-class  K20025Diary(AbstractDiary):
-    def get_date(self):
-        return "2021-12-09"
-    def get_summary(self):
-        return "ご飯がおいしかった"
-    def get_author(self):
-        return "K20025"
--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from diaries.K21014Diary import K21014Diary
 from diaries.SomekiDiary import SomekiDiary
 from diaries.MuramatsuDiary import MuramatsuDiary
 from diaries.Diary_k21091 import Diary_k21091
-from diaries.k21054Diary import K21054Diary
-from diaries.K20025Diary import K20025Diary
+from diaries.K21054Diary import K21054Diary
 
 #↓のリストには、メンバーの各日記が格納されます。
 diaries = [
@@ -14,7 +13,6 @@ diaries = [
     MuramatsuDiary(),
     Diary_k21091(),
     K21054Diary(),
-    K20025Diary()
 ]
 
 
